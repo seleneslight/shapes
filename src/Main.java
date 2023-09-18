3875cd8c8f9f01e085bfbfc8ad03ff374fef57a5
seleneslight <jkantorowicz05@gmail.com>
2023-09-18 09:20:22 +0200

    added some optimization

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,35 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+    public static void DisplayOptions() {
+        System.out.println("1. Okrąg");
+        System.out.println("2. Kwadrat");
+        System.out.println("3. Trójkąt");
+    }
     public static void main(String[] args) {
         boolean run = true;
         while (run) {
             System.out.println("Jaką figurę chcesz stworzyć?");
+            DisplayOptions();
             Scanner scanner = new Scanner(System.in);
-            System.out.println("1. Okrąg");
-            System.out.println("2. Kwadrat");
-            System.out.println("3. Trójkąt");
             try {
                 int respondA = scanner.nextInt();
                 switch (respondA) {
-                    case 1:
+                    case 1 -> {
                         System.out.println("Podaj promień tego okręgu.");
                         double r = scanner.nextDouble();
                         Circle circle = new Circle(r);
                         System.out.println("Pole okręgu wynosi: " + circle.shapeArea()
                                 + "        A jego obwód wynosi:" + circle.shapeCircumference());
-                        break;
-                    case 2:
+                    }
+                    case 2 -> {
                         System.out.println("Podaj długość boku tego kwadratu.");
                         double s = scanner.nextDouble();
                         Square square = new Square(s);
                         System.out.println("Pole kwadratu wynosi: " + square.shapeArea()
                                 + "        A jego obwód wynosi:" + square.shapeCircumference());
-                        break;
-                    case 3:
+                    }
+                    case 3 -> {
                         System.out.println("Podaj długości boków tego trójkąta.");
                         System.out.println("A:");
                         double a = scanner.nextDouble();
@@ -38,10 +41,11 @@ public class Main {
                         Triangle triangle = new Triangle(a, b, c);
                         System.out.println("Pole trójkąta wynosi: " + triangle.shapeArea()
                                 + "        A jego obwód wynosi:" + triangle.shapeCircumference());
-                        break;
-                    default:
+                    }
+                    default -> {
                         System.out.println("Podaj wartość 1-3");
                         continue;
+                    }
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Podaj wartość numeryczną");
